48aacc3bf74b020d9e0bd0bef3e6686bff5e5b86
Nerontronik <stefchicomin@gmail.com>
2022-08-30 19:22:13 -0500

    añadi comentarios de los comandos

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
 int main (void)
 {
     printf("hola mi gente");
+    // git clone
+    // git fetch
+    // git pull origin main
+    // git commit -am "mensaje"
+    // git push origin main
+    // 
+    //
 }
